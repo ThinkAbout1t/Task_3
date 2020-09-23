c6d6d34f3d844b0f1f6b69c2730d1820585e6298
pakhomov <pakhomov.o@donnu.edu.ua>
2020-09-23 12:26:17 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Flag = true;
+            while (Flag == true)
+            {
+                try
+                {
+                    Console.OutputEncoding = System.Text.Encoding.UTF8;
+                    Console.Write("введіть змінну типу int: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("введіть змінну типу double: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("введіть змінну типу long: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+
+                    Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    Flag = true;
+                    Console.WriteLine("Enter correct value");
+                }
+            }
+        }
+    }
+}
