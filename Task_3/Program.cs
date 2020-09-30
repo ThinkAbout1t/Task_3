38e613c6908686c11de69fbf48bfb1a56076b2d8
pakhomov <pakhomov.o@donnu.edu.ua>
2020-09-30 12:58:38 +0300

    coomit3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -10,30 +10,56 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            bool Flag = true;
-            while (Flag == true)
+            int a;
+            double b;
+            long c;
+            while (true)
             {
                 try
                 {
-                    Console.OutputEncoding = System.Text.Encoding.UTF8;
-                    Console.Write("введіть змінну типу int: ");
-                    int a = Convert.ToInt32(Console.ReadLine());
-
-                    Console.Write("введіть змінну типу double: ");
-                    double b = Convert.ToDouble(Console.ReadLine());
-
-                    Console.Write("введіть змінну типу long: ");
-                    long c = Convert.ToInt64(Console.ReadLine());
-
-                    Console.WriteLine($"a = {a}; b = {b}; c = {c}");
-                    Console.ReadKey();
+                    Console.Write("(int) a = ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    break;
                 }
+
                 catch
                 {
-                    Flag = true;
-                    Console.WriteLine("Enter correct value");
+                    Console.WriteLine("It is not int. Input again.");
                 }
             }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("(double) b = ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+
+                catch
+                {
+                    Console.WriteLine("It is not double. Input again.");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("(long) c = ");
+                    c = Convert.ToInt64(Console.ReadLine());
+                    break;
+                }
+
+                catch
+                {
+                    Console.WriteLine("It is not long. Input again.");
+                }
+            }
+
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            Console.ReadKey();
         }
     }
 }
